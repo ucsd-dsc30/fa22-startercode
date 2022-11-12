907bc8c26df0d92af5079dd47f20a95cc2d98134
Qcallahan <quentin.callahan2003@gmail.com>
2022-11-12 00:13:29 -0800

    added new startercode for PA8

---
+++ PA8/Restaurant.java
@@ -0,0 +1,41 @@
+/*
+ * Name: SOLUTION
+ * PID: A12345678
+ */
+
+import java.util.ArrayList;
+
+/**
+ * Solution implementation of the PA8 restaurant class
+ *
+ * @author Quentin Callahan
+ * @since 11/11/2022
+ */
+
+public class Restaurant {
+    ArrayList<HashTable> recipes;
+    HashTable inventory;
+
+    public Restaurant() {
+        /* TODO */
+    }
+
+    public boolean addIngredient(String ingredient) {
+        /* TODO */
+        return false;
+    }
+
+    public void addRecipe(String[] ingredients) {
+        /* TODO */
+    }
+
+    public boolean checkIngredients(String[] ingredients) {
+        /* TODO */
+        return false;
+    }
+
+    public int recommend(String[] likes, String[] dislikes) {
+        /* TODO */
+        return -1;
+    }
+}
